6596154e220feba682a739592602f08bb97b20b3
zyzcfzs <zyzcfzs@163.com>
2022-05-02 19:16:45 -0400

    remove encrypted file

--- run_onchange_install.sh
+++ run_onchange_install.sh
@@ -9,7 +9,7 @@ function common_set_up() {
     fi
     if [[ ! -d ~/.tmux/plugins/tpm ]]; then
         git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-        ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+        echo 'Use Ctrl + z + I to Install tmux packages'
     fi
     if [[ ! -f "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim ]]; then
         sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
