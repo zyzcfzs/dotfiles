3e510815157e3404533dc47f0bc6f679ad6a603b
WTF <71304137+zyzcfzs@users.noreply.github.com>
2023-06-04 01:19:51 -0400

    Update run_onchange_install.sh

--- run_onchange_install.sh
+++ run_onchange_install.sh
@@ -4,9 +4,6 @@ function common_set_up() {
     if [[ ! -f ~/.config/antigen.zsh ]]; then
         curl -L https://git.io/antigen > ~/.config/antigen.zsh
     fi
-    if [[ ! -d ~/.oh-my-zsh ]]; then
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    fi
     if [[ ! -d ~/.tmux/plugins/tpm ]]; then
         git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
         echo 'Use Ctrl + z + I to Install tmux packages'
@@ -15,11 +12,14 @@ function common_set_up() {
         sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
         nvim -c 'PlugInstall'
     fi
+    if [[ ! -d ~/.oh-my-zsh ]]; then
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    fi
 }
 if [[ "$(which pacman >/dev/null; echo -n $?)" == "0" ]]; then 
-    sudo pacman -Syyu && sudo pacman --needed -S fd xclip nodejs npm alacritty neovim curl wget aria2 tmux zsh ripgrep bat zoxide fzf exa picom nitrogen yarn && common_set_up
+    sudo pacman -Syyu && sudo pacman --needed -S fd xclip nodejs npm alacritty neovim curl wget aria2 tmux zsh ripgrep bat zoxide fzf exa picom nitrogen yarn
 elif [[ "$(which apt >/dev/null; echo -n $?)" == "0" ]]; then 
-    sudo apt update && sudo apt dist-upgrade -y && sudo apt install -y fd-find xclip nodejs npm zsh tmux curl wget aria2 fzf bat ripgrep neovim tmux exa picom nitrogen zoxide && common_set_up
+    sudo apt update && sudo apt dist-upgrade -y && sudo apt install -y fd-find xclip nodejs npm zsh tmux curl wget aria2 fzf bat ripgrep neovim tmux exa picom nitrogen zoxide
     if [[ "$(which yarn >/dev/null; echo -n $?)" != "0" ]]; then 
         sudo npm install -g yarn
     fi
@@ -30,3 +30,4 @@ elif [[ "$(which apt >/dev/null; echo -n $?)" == "0" ]]; then
         sudo mv $(which fdfind) $(dirname $(which fdfind))"/fd"
     fi
 fi
+common_set_up
