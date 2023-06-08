ef8a2785b01365a5b9c8eacaddc01e3fcf749e9f
WTF <71304137+zyzcfzs@users.noreply.github.com>
2023-06-08 19:17:24 -0400

    Update run_onchange_install.sh

--- run_onchange_install.sh
+++ run_onchange_install.sh
@@ -8,18 +8,15 @@ function common_set_up() {
         git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
         echo 'Use Ctrl + z + I to Install tmux packages'
     fi
-    if [[ ! -f "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim ]]; then
-        sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-        nvim -c 'PlugInstall'
-    fi
     if [[ ! -d ~/.oh-my-zsh ]]; then
         sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     fi
+    curl https://raw.githubusercontent.com/zyzcfzs/dotfiles/main/dot_zshrc > ~/.zshrc
 }
 if [[ "$(which pacman >/dev/null; echo -n $?)" == "0" ]]; then 
-    sudo pacman -Syyu && sudo pacman --needed -S fd xclip nodejs npm alacritty neovim curl wget aria2 tmux zsh ripgrep bat zoxide fzf exa picom nitrogen yarn
+    sudo pacman -Syyu && sudo pacman --needed -S fd xclip nodejs npm alacritty curl wget aria2 tmux zsh ripgrep bat zoxide fzf exa picom nitrogen yarn
 elif [[ "$(which apt >/dev/null; echo -n $?)" == "0" ]]; then 
-    sudo apt update && sudo apt dist-upgrade -y && sudo apt install -y fd-find xclip nodejs npm zsh tmux curl wget aria2 fzf bat ripgrep neovim tmux exa picom nitrogen zoxide
+    sudo apt update && sudo apt dist-upgrade -y && sudo apt install -y fd-find xclip nodejs npm zsh tmux curl wget aria2 fzf bat ripgrep tmux exa picom nitrogen zoxide
     if [[ "$(which yarn >/dev/null; echo -n $?)" != "0" ]]; then 
         sudo npm install -g yarn
     fi
