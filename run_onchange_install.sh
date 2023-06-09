c90113ef275b22f8ebe0026fb9fcfa8e941ac289
WTF <71304137+zyzcfzs@users.noreply.github.com>
2023-06-09 17:41:16 -0400

    Update run_onchange_install.sh

--- run_onchange_install.sh
+++ run_onchange_install.sh
@@ -12,11 +12,12 @@ function common_set_up() {
         sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     fi
     curl https://raw.githubusercontent.com/zyzcfzs/dotfiles/main/dot_zshrc > ~/.zshrc
+    curl https://gist.githubusercontent.com/zyzcfzs/33e7940448456aafc8c7973fdb886a2c/raw/d25cab7f91e655c8a52fd065eb8f38d37c65b349/.vimrc > ~/.vimrc
 }
 if [[ "$(which pacman >/dev/null; echo -n $?)" == "0" ]]; then 
-    sudo pacman -Syyu && sudo pacman --needed -S fd xclip nodejs npm alacritty curl wget aria2 tmux zsh ripgrep bat zoxide fzf exa picom nitrogen yarn
+    sudo pacman -Syyyu && sudo pacman --needed -S fd xclip nodejs npm alacritty curl wget aria2 tmux zsh ripgrep bat zoxide fzf exa yarn
 elif [[ "$(which apt >/dev/null; echo -n $?)" == "0" ]]; then 
-    sudo apt update && sudo apt dist-upgrade -y && sudo apt install -y fd-find xclip nodejs npm zsh tmux curl wget aria2 fzf bat ripgrep tmux exa picom nitrogen zoxide
+    sudo apt update && sudo apt dist-upgrade -y && sudo apt install -y fd-find xclip nodejs npm zsh tmux curl wget aria2 fzf bat ripgrep tmux exa zoxide
     if [[ "$(which yarn >/dev/null; echo -n $?)" != "0" ]]; then 
         sudo npm install -g yarn
     fi
