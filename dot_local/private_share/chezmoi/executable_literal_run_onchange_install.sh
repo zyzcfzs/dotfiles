138b4c91e3a4f36428e67778b9d495213b6776fb
WTF <71304137+zyzcfzs@users.noreply.github.com>
2023-06-09 18:11:55 -0400

    Update executable_literal_run_onchange_install.sh

--- dot_local/private_share/chezmoi/executable_literal_run_onchange_install.sh
+++ dot_local/private_share/chezmoi/executable_literal_run_onchange_install.sh
@@ -4,22 +4,20 @@ function common_set_up() {
     if [[ ! -f ~/.config/antigen.zsh ]]; then
         curl -L https://git.io/antigen > ~/.config/antigen.zsh
     fi
-    if [[ ! -d ~/.oh-my-zsh ]]; then
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    fi
     if [[ ! -d ~/.tmux/plugins/tpm ]]; then
         git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
         echo 'Use Ctrl + z + I to Install tmux packages'
     fi
-    if [[ ! -f "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim ]]; then
-        sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-        nvim -c 'PlugInstall'
+    if [[ ! -d ~/.oh-my-zsh ]]; then
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     fi
+    curl https://raw.githubusercontent.com/zyzcfzs/dotfiles/main/dot_zshrc > ~/.zshrc
+    curl https://gist.githubusercontent.com/zyzcfzs/33e7940448456aafc8c7973fdb886a2c/raw/d25cab7f91e655c8a52fd065eb8f38d37c65b349/.vimrc > ~/.vimrc
 }
 if [[ "$(which pacman >/dev/null; echo -n $?)" == "0" ]]; then 
-    sudo pacman -Syyu && sudo pacman --needed -S fd xclip nodejs npm alacritty neovim curl wget aria2 tmux zsh ripgrep bat zoxide fzf exa picom nitrogen && common_set_up
+    sudo pacman -Syyyu && sudo pacman --needed -S fd xclip alacritty curl wget aria2 tmux zsh ripgrep bat zoxide fzf exa yarn
 elif [[ "$(which apt >/dev/null; echo -n $?)" == "0" ]]; then 
-    sudo apt update && sudo apt dist-upgrade -y && sudo apt install -y fd-find xclip nodejs npm zsh tmux curl wget aria2 fzf bat ripgrep neovim tmux exa picom nitrogen zoxide && common_set_up
+    sudo apt update && sudo apt dist-upgrade -y && sudo apt install -y fd-find xclip zsh tmux curl wget aria2 fzf bat ripgrep tmux exa zoxide
     if [[ "$(which batcat >/dev/null; echo -n $?)" == "0" ]]; then 
         sudo mv $(which batcat) $(dirname $(which batcat))"/bat"
     fi
@@ -27,3 +25,4 @@ elif [[ "$(which apt >/dev/null; echo -n $?)" == "0" ]]; then
         sudo mv $(which fdfind) $(dirname $(which fdfind))"/fd"
     fi
 fi
+common_set_up
